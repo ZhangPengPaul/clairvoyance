6041a0ed3c7d4889ea90400a3b2154b3d4992b55
guoqing.ma <guoqing.ma@dajie-inc.com>
2016-11-24 11:36:09 +0800

    add maven

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 time  rm -rf lib/ target/
 mkdir lib
-cp  $PLAY_HOME/framework/play-1.4.1.jar lib/
+cp  $PLAY_HOME/framework/play*.jar lib/
 time  mvn clean dependency:copy-dependencies -DoutputDirectory=lib
