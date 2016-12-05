8370391aa804d9727ecedd7c6a92bb6ea5298939
ggh <ggh666@163.com>
2016-12-05 14:02:06 +0800

    添加es方法

---
+++ app/es/common/ElasticClient.java
@@ -0,0 +1,194 @@
+package es.common;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import es.po.AggsPo;
+import io.searchbox.client.JestClient;
+import io.searchbox.client.JestClientFactory;
+import io.searchbox.client.JestResult;
+import io.searchbox.client.config.HttpClientConfig;
+import io.searchbox.core.*;
+import io.searchbox.core.search.aggregation.TermsAggregation;
+import io.searchbox.indices.CreateIndex;
+import io.searchbox.indices.aliases.AddAliasMapping;
+import io.searchbox.indices.aliases.ModifyAliases;
+import io.searchbox.indices.aliases.RemoveAliasMapping;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author guanghui
+ * @version 1.0
+ * @since 16/11/25 上午10:05
+ */
+public class ElasticClient {
+
+    private static Logger logger = LoggerFactory.getLogger(ElasticClient.class);
+    private static boolean isMultiThreaded = true;
+    private static JestClient client;
+    private static ExecutorService executorService = Executors.newFixedThreadPool(16);
+
+    static {
+        Set<String> urls = new HashSet<>();
+        urls.add("http://localhost:9205");
+//        urls.add("http://10.1.21.85:9200");
+        JestClientFactory factory = new JestClientFactory();
+        HttpClientConfig config = new HttpClientConfig.Builder(urls).maxTotalConnection(100)
+                .defaultMaxTotalConnectionPerRoute(1000).multiThreaded(isMultiThreaded).connTimeout(10000)
+                .readTimeout(1000000).build();
+        factory.setHttpClientConfig(config);
+        client = factory.getObject();
+    }
+
+    public static JestClient getClient() {
+        return client;
+    }
+
+    public static void createDocuments(Object docObject, String indexName, String typeName) {
+        Index index = new Index.Builder(docObject).index(indexName).type(typeName).build();
+        try {
+            JestResult result = client.execute(index);
+        } catch (IOException e) {
+            e.printStackTrace();
+            logger.error("", e);
+        }
+    }
+
+    public static void updateDocuments(String docId, String updateScript, String indexName, String typeName) {
+        try {
+            Update update = new Update.Builder(updateScript).index(indexName).type(typeName).id(docId).build();
+            JestResult rs = client.execute(update);
+            System.out.println(rs.getJsonString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleltDocuments(String docId, String indexName, String typeName) {
+        try {
+            client.execute(new Delete.Builder(docId).index(indexName).type(typeName).build());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static JestResult getDocument(String docId, String indexName, String typeName) {
+        JestResult result = null;
+        try {
+            result = client.execute(new Get.Builder(indexName, docId).type(typeName).build());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+    public static JsonObject searchByAggs(String aggsField, int pageNum, int pageSize, String indexName, String typeName) throws IOException {
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+//        String query = "";
+//        String project = "";
+//        BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
+//        boolQuery.must((QueryBuilders.matchQuery("code", query)));
+//        boolQuery.must(QueryBuilders.matchQuery("project", project));
+//        searchSourceBuilder.query(boolQuery);
+
+        AggregationBuilder aggregation = AggregationBuilders.terms("aggs").field(aggsField).size(20);
+        searchSourceBuilder.aggregation(aggregation);
+        int from = 0;
+        if (pageNum > 1) {
+            from = (pageNum - 1) * pageSize;
+        }
+        searchSourceBuilder.size(pageSize);
+        searchSourceBuilder.from(from);
+
+        Search search = new Search.Builder(searchSourceBuilder.toString()).addIndex(indexName).addType(typeName).build();
+
+        SearchResult result = ElasticClient.getClient().execute(search);
+        if (result.isSucceeded()) {
+            JsonArray asJsonArray = result.getJsonObject().getAsJsonObject("hits").getAsJsonArray("hits");
+            JsonObject resultJson = new JsonObject();
+
+            resultJson.add("result", asJsonArray);
+            resultJson.addProperty("total", result.getJsonObject().getAsJsonObject("hits").get("total").getAsInt());
+
+            TermsAggregation terms = result.getAggregations().getTermsAggregation("aggs");
+            StringBuilder aggsResult = new StringBuilder();
+            Gson gson = new Gson();
+            JsonArray aggsArray = new JsonArray();
+            for (TermsAggregation.Entry e : terms.getBuckets()) {
+                AggsPo aggsPo = new AggsPo();
+                aggsPo.setProjectName(e.getKey());
+                aggsPo.setDocCount(e.getCount());
+                aggsArray.add(gson.toJsonTree(aggsPo));
+            }
+
+            resultJson.add("aggs", aggsArray);
+            return resultJson;
+        }
+        return null;
+    }
+
+
+    public static JsonObject search(SearchSourceBuilder builder, String index, String type) {
+        Search search = new Search.Builder(builder.toString()).addIndex(index).addType(type).build();
+        JestResult result;
+        try {
+            result = client.execute(search);
+        } catch (IOException e) {
+            logger.error("Execute search error.", e);
+            throw new RuntimeException("Execute search error.", e);
+        }
+        if (!result.isSucceeded()) {
+            logger.error("Execute search failure.");
+            throw new RuntimeException(result.getErrorMessage());
+        }
+
+        JsonArray asJsonArray = result.getJsonObject().getAsJsonObject("hits").getAsJsonArray("hits");
+        JsonObject resultJson = new JsonObject();
+        JsonArray resultArray = new JsonArray();
+        JsonObject json;
+        JsonObject source;
+        JsonObject highLight;
+        for (int i = 0; i < asJsonArray.size(); i++) {
+            json = asJsonArray.get(i).getAsJsonObject();
+            source = json.getAsJsonObject("_source");
+            highLight = json.getAsJsonObject("highlight");
+            if (null != highLight) {
+
+            }
+            source.add("_type", json.get("_type"));
+            source.addProperty("_order", i);
+            resultArray.add(source);
+        }
+
+        resultJson.addProperty("total", result.getJsonObject().getAsJsonObject("hits").get("total").getAsInt());
+        resultJson.add("data", resultArray);
+        return resultJson;
+    }
+
+    public static String explain(SearchSourceBuilder builder, String id, String index, String type) {
+        Explain search = new Explain.Builder(index, type, id, builder.toString()).build();
+        JestResult result;
+        try {
+            result = client.execute(search);
+        } catch (IOException e) {
+            logger.error("Execute explain error.", e);
+            throw new RuntimeException("Execute explain error.", e);
+        }
+        if (!result.isSucceeded()) {
+            logger.error("Execute explain failure.");
+            throw new RuntimeException(result.getErrorMessage());
+        }
+        return result.getJsonObject().toString();
+    }
+}
